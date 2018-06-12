c8fbfd366b9e6014e797a2fa4aa721573ddb5b36
Nuno Carriço <nuno.carrico.98@gmail.com>
2018-06-12 18:11:40 +0100

    Add start of lower interface

--- Roguelike/Roguelike/Trap.cs
+++ Roguelike/Roguelike/Trap.cs
@@ -12,7 +12,7 @@ namespace Roguelike
         public float Damage { get; private set; }
         public TypesOfTraps TrapType { get; set; }
         public Position TrapPos { get; set; }
-        Random rnd = new Random();
+        Random rnd = new Random(Guid.NewGuid().GetHashCode());
 
         public Trap(Position pos)
         {
