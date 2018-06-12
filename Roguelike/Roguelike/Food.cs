acfb3c0b73eb1c122aa4db4dd31d5eb5b64ff6ee
MlgMaia <Diogo_maia99@hotmail.com>
2018-06-12 17:36:11 +0100

    Add TypesOfFood and food class

---
+++ Roguelike/Roguelike/Food.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    class Food : Item
+    {
+        public float HPIncrease { get; private set; }
+        public TypesOfFood FoodType { get; set; }
+        public Position FoodPos { get; set; }
+        Random rnd = new Random();
+
+        public Food(Position Pos)
+        {
+            FoodType = (TypesOfFood)rnd.Next(0, Enum.GetNames(typeof(TypesOfTraps)).Length + 1);
+            SetHp();
+            FoodPos = Pos;
+        }
+
+        public Food(TypesOfFood typefood)
+        {
+            FoodType = typefood;
+            SetHp();
+        }
+
+        public void SetHp()
+        {
+            switch (FoodType)
+            {
+                case TypesOfFood.Bacon:
+                    HPIncrease = 4;
+                    break;
+                case TypesOfFood.Burger:
+                    HPIncrease = 10;
+                    break;
+                case TypesOfFood.Noodles:
+                    HPIncrease = 7;
+                    break;
+                case TypesOfFood.Pizza:
+                    HPIncrease = 8;
+                    break;
+                case TypesOfFood.Sushi:
+                    HPIncrease = 2;
+                    break;
+            }
+
+        }
+        public override string ToString()
+        {
+
+            return ($"{FoodType}| {HPIncrease}");
+        }
+    }
+}
---
+++ Roguelike/Roguelike/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    class Weapon : Item
+    {
+        public float AttackPower { get; private set; }
+        public double Durability { get; private set; }    
+    }
+}
