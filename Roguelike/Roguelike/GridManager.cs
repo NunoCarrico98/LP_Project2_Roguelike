81c61034f1cb9d2756d32dff9c04c97bd9be647c
Insoel <insoel@gmail.com>
2018-06-13 15:25:52 +0100

    Improve traps

--- Roguelike/Roguelike/GridManager.cs
+++ Roguelike/Roguelike/GridManager.cs
@@ -87,7 +87,7 @@ namespace Roguelike
         {
             // Remove Player from current tile
             gameGrid[oldPlayerPos.X, oldPlayerPos.Y].Remove(player);
-            gameGrid[oldPlayerPos.X, oldPlayerPos.Y].Insert(0, null);
+            gameGrid[oldPlayerPos.X, oldPlayerPos.Y].Add(null);
             oldPlayerPos = new Position(player.PlayerPos.X, player.PlayerPos.Y);
 
             // Add Player to new tile
@@ -105,9 +105,11 @@ namespace Roguelike
             for (int i = ObjectsPerTile - 1; i >= 0; i--)
             {
                 IGameObject go = gameGrid[player.PlayerPos.X, player.PlayerPos.Y][i];
-                if (go is Trap)
+                if (go is Trap && (go as Trap).FallenInto == false)
                 {
+                    (go as Trap).FallenInto = true;
                     player.Health -= (go as Trap).Damage;
+
 
                 }
             }
